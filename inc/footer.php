7c5a0c439f8bac6ddc3569f5bafe71c0a4b1503a
JoskaGyuricza <joska.gyur@gmail.com>
2021-05-03 11:22:02 +0200

    ajout header et footer

---
+++ inc/footer.php
@@ -0,0 +1,15 @@
+<!-- footer !-->
+<footer class="footer_complete">
+
+    <p class="copyright">Copyright © Joska Gyuricza 2019 - Tous droits réservés</p>
+    <div class="footer_social">
+        <a href="info_film.php"><img src="images/logo/logo_twitter.png" alt="twitter"></a>
+        <a href="info_film.php"><img src="images/logo/logo_fb.png" alt="Facebook"></a>
+    </div>
+
+
+</footer>
+
+
+</body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet"  href="css/reset.css">
+    <link rel="stylesheet"   href="css/style_mobile.css">
+    <link rel="stylesheet"  media="screen and (min-width: 500px)" href="css/style_tablet.css">
+    <link rel="stylesheet"  media="screen and (min-width: 769px)"  href="css/style_V2.css">
+
+
+
+    <title>festival des Films de Plein air</title>
+</head>
+
+<body>
+<!-- Barre de navigation principale Barre de navigation principale !  ( à fixer avec le CSS ) !-->
+<header class="navbar_complete">
+    <!-- ajouter titre du festival en <h1> quelque part !-->
+    <a class="navbar_brand" href="index.php"><img src="images/logo/logo_festival2.png" alt="logo du festival"/></a>
+
+    <nav class="navbar_collapse">
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="selection.php">Séléction</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="actualites.php">Actualités</a></li>
+        </ul>
+    </nav>
+
+
+</header>
+
+
